5fc028068552036140160dbcdafd8af53e4843fd
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-17 12:18:52 -0300

    feat: created DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,9 @@ public class Program {
 	//	System.out.println(obj);
 		
 		Seller sell = new Seller (1, "Vendas", "funcionario@email.com", new Date(), 3000f, new Department(1, "Books"));
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(sell);
 	}
 }
