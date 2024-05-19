8f4c97e3fc5cf5dec271b67acf13566bc777510a
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-19 08:42:12 -0300

    feat: created findByDepartment function

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -14,10 +14,15 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("== Test 01 - Seller findById ==");
-		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller); 
 		
+		System.out.println("== Test 02 - Seller findByDepartment ==");
+		Department dept = new Department(1, null);
+		List<Seller> list = sellerDao.findByDepartment(dept);
+		
+		for (Seller c : list) {
+			System.out.println(c);
+		}
 	}
 }
