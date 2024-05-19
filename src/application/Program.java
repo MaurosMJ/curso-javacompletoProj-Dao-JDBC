986003ddd5dc6bc2aadd5286a43fbbfd737ac7fa
Mauros Milach Junior <mauros99milach@gmail.com>
2024-05-19 11:14:12 -0300

    feat: created update statement (class seller)

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,11 +32,17 @@ public class Program {
 		for (Seller c : list) {
 			System.out.println(c);
 		}
-		
+/*		
 		System.out.println("== Test 04 - Seller Insert ==");
 		seller = new Seller(1, "Greg", "greg@email.com", new Date(), 4000f, dept);
 		sellerDao.insert(seller);
 		System.out.println("Inserted! New Id = " + seller.getId());
+	*/	
+		System.out.println("== Test 05 - Seller Update ==");
+		seller = sellerDao.findById(1);
+		seller.setName("Bruce Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed.");
 		
 	}
 }
